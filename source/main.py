ec8b893c1c6978374b560ffe7b8e7f0e8fca1647
nullbyte <bytenulo@tutamail.com>
2026-07-08 02:24:18 -0300

    The vulnerability related to credential exposure has been fixed; CSRF protection and other security measures will be included in the next update

--- source/main.py
+++ source/main.py
@@ -1,14 +1,20 @@
 from model import db,app
 from routes.public import public_bp 
 from routes.admin import admin_bp
+import json
 
-hosting = "localhost"
-porting = 8080
+with open("config.json","r",encoding="utf-8") as config:
+    data = json.load(config) 
+
+
+hosting = data["hosting"]
+porting = data["porting"]
+secret_key = data["secret_key"]
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.secret_key = "3905ed174eca6e6d443ccfb4b9b1c77b430ea2b800ccec9c5a3c2ae3e9378b5e"
+    app.secret_key = secret_key 
     app.register_blueprint(public_bp)
     app.register_blueprint(admin_bp,url_prefix="/admin")
     app.run(debug=True,host=hosting,port=porting)
